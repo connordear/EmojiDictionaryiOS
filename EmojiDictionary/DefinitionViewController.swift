1b0cc977ea4c5a2ec6674d41ea0a927860dbd293
Connor Dear <connordear@gmail.com>
2017-04-14 15:56:02 -0400

    added 5 working examples

--- EmojiDictionary/DefinitionViewController.swift
+++ EmojiDictionary/DefinitionViewController.swift
@@ -12,11 +12,26 @@ class DefinitionViewController: UIViewController {
 
     var emoji = "No Emoji"
     @IBOutlet weak var EmojiLabel: UILabel!
-    
+    @IBOutlet weak var DefinitionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         EmojiLabel.text = emoji
         // Do any additional setup after loading the view.
+        if EmojiLabel.text == "😀" {
+            DefinitionLabel.text = "The classic Smiley."
+        } else if EmojiLabel.text == "😈"{
+            DefinitionLabel.text = "The purple Devil face!"
+        } else if EmojiLabel.text == "👌"{
+            DefinitionLabel.text = "Giving you the 'alright!'"
+        } else if EmojiLabel.text == "👅"{
+            DefinitionLabel.text = "Sticking out the tongue."
+        } else if EmojiLabel.text == "👨‍❤️‍👨"{
+            DefinitionLabel.text = "Couple in love!"
+        }
+
+
+
+        
     }
 
     override func didReceiveMemoryWarning() {
